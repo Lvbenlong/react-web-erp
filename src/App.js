27aa4059d5ebf6a67193ea4f3f1d1983833d7aa6
bestkit <bestkit@bestkitdeiMac.local>
2019-05-14 11:24:38 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, NavLink, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Home
     </div>
   );
+}
+
+function About() {
+  return (
+    <div>
+      About
+    </div>
+  );
+}
+
+function B() {
+  return (
+    <div>
+      B
+    </div>
+  );
+}
+
+
+class App extends React.Component {
+  render() {
+    const activeStyle = {
+      color: 'yellow',
+      fontSize: '26px',
+    };
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Link to="/home">
+            home
+          </Link>
+          <NavLink exact to="/about" activeClassName="selected" activeStyle={activeStyle}>
+          about
+          </NavLink>
+          <NavLink to="/b" activeClassName="selected" activeStyle={activeStyle}>
+            BBB
+          </NavLink>
+          <Route exact path="/" component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/b" component={B} />
+
+        </header>
+      </div>
+
+    );
+  }
 }
 
 export default App;
